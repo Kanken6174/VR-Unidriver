98285bc437d66cd8a4edd32987e98f6e2c7c67b3
Elliott Le Guehennec <elliott.leguehennec@gmail.com>
2021-11-15 15:34:04 +0100

    commented class NumberComparator

--- code_tests/TestApp_ELG_sem2/TestApp_ELG_sem2/NumberComparator.cpp
+++ code_tests/TestApp_ELG_sem2/TestApp_ELG_sem2/NumberComparator.cpp
@@ -1,5 +1,8 @@
+// Authors : Elliott
+// Documentation date : 15/11
 #include "NumberComparator.h"
 
+// see NumberComparator.h
 int NumberComparator::getMax(int a, int b) {
 	return a > b ? a : b;
 }
