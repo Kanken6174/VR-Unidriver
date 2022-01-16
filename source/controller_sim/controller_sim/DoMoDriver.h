58f77639059b05123f3056dc824d4e449f8f9460
Onyx ab3q <Onyxab3q@outlook.com>
2022-01-16 20:06:16 +0100

    XML documentation work

--- source/controller_sim/controller_sim/DoMoDriver.h
+++ source/controller_sim/controller_sim/DoMoDriver.h
@@ -38,66 +38,96 @@ private:
 	std::vector <int> componentType;	//ancienne façon de créer les composants en précisant leur type pour chaque, obsolète
 
 public:
-	/// <summary>
-	/// Le constructeur d'une classe DoMoDriver (version moderne, désactive le mode obsolète si il est appellé)
-	/// </summary>
-	/// <param name="driverDataTemplate">La data Template de ce driver, qui décrit l'appareil que ce driver représente</param>
-	DoMoDriver(DriverDataTemplate driverDataTemplate);
 
-	//constructeur Obsolète
-	[[deprecated("Ancienne version qui n'utilise pas les DataTemplate, à éviter")]]
-	DoMoDriver(std::string name);
-	//Constructeur Obsolète
-	[[deprecated("Ancienne version qui n'utilise pas les DataTemplate, à éviter")]]
-	DoMoDriver(std::string name, std::vector<int> compoTypes, std::vector<std::string> compoPaths);
+	///Constructeurs et destructeurs
+		/// <summary>
+		/// Le constructeur d'une classe DoMoDriver (version moderne, désactive le mode obsolète si il est appellé)
+		/// </summary>
+		/// <param name="driverDataTemplate">La data Template de ce driver, qui décrit l'appareil que ce driver représente</param>
+		DoMoDriver(DriverDataTemplate driverDataTemplate);
 
-	/// <summary>
-	/// Le destructeur de ce driver
-	/// </summary>
-	virtual ~DoMoDriver();
+		//constructeur Obsolète
+		[[deprecated("Ancienne version qui n'utilise pas les DataTemplate, à éviter")]]
+		DoMoDriver(std::string name);
+		//Constructeur Obsolète
+		[[deprecated("Ancienne version qui n'utilise pas les DataTemplate, à éviter")]]
+		DoMoDriver(std::string name, std::vector<int> compoTypes, std::vector<std::string> compoPaths);
+
+		/// <summary>
+		/// Le destructeur de ce driver
+		/// </summary>
+		virtual ~DoMoDriver();
 
 	//Ces 4 fonctions servent juste à factoriser celles données par openVR pour modifier des valeurs
-	virtual void setStrProperty(vr::ETrackedDeviceProperty SVRproperty, std::string value);
-	virtual void setInt32Property(vr::ETrackedDeviceProperty SVRproperty, int32_t value);
-	virtual void setUInt64Property(vr::ETrackedDeviceProperty SVRproperty, uint64_t value);
-	virtual void setBoolProperty(vr::ETrackedDeviceProperty SVRproperty, bool value);
+		virtual void setStrProperty(vr::ETrackedDeviceProperty SVRproperty, std::string value);
+		virtual void setInt32Property(vr::ETrackedDeviceProperty SVRproperty, int32_t value);
+		virtual void setUInt64Property(vr::ETrackedDeviceProperty SVRproperty, uint64_t value);
+		virtual void setBoolProperty(vr::ETrackedDeviceProperty SVRproperty, bool value);
 
+	///Fonctions d'activation du driver SteamVR
+		/// <summary>
+		/// Cette fonction va activer et enregistrer le driver auprès de SteamVR, à partir des DataTemplates enregistrées en interne
+		/// </summary>
+		/// <param name="unObjectId"></param>
+		/// <returns></returns>
+		virtual EVRInitError ModernActivation(vr::TrackedDeviceIndex_t unObjectId);
 
-	virtual EVRInitError ModernActivation(vr::TrackedDeviceIndex_t unObjectId);
-
-	//Activation obsolète
-	virtual EVRInitError [[deprecated("Ancienne version qui n'utilise pas les DataTemplate, à éviter")]] ObsoleteActivation(vr::TrackedDeviceIndex_t unObjectId);
+		//Fonction d'activation obsolète
+		virtual EVRInitError [[deprecated("Ancienne version qui n'utilise pas les DataTemplate, à éviter")]] ObsoleteActivation(vr::TrackedDeviceIndex_t unObjectId);
 
 	virtual void registerProperties(vr::TrackedDeviceIndex_t unObjectId);
 
-	//fonctions requises par l'interface ITrackedDeviceServerDriver, commes données dans le sample, elles ne sont pas utilisées
-	[[maybe_unused]] virtual EVRInitError Activate(vr::TrackedDeviceIndex_t unObjectId);
-	[[maybe_unused]] virtual void Deactivate();
-	[[maybe_unused]] virtual void EnterStandby();
-	[[maybe_unused]] virtual void* GetComponent(const char* pchComponentNameAndVersion);
-	[[maybe_unused]] virtual void PowerOff();
-	[[maybe_unused]] virtual void DebugRequest(const char* pchRequest, char* pchResponseBuffer, uint32_t unResponseBufferSize);
+	///fonctions requises par l'interface ITrackedDeviceServerDriver, commes données dans le sample, elles ne sont pas utilisées
+		[[maybe_unused]] virtual EVRInitError Activate(vr::TrackedDeviceIndex_t unObjectId);
+		[[maybe_unused]] virtual void Deactivate();
+		[[maybe_unused]] virtual void EnterStandby();
+		[[maybe_unused]] virtual void* GetComponent(const char* pchComponentNameAndVersion);
+		[[maybe_unused]] virtual void PowerOff();
+		[[maybe_unused]] virtual void DebugRequest(const char* pchRequest, char* pchResponseBuffer, uint32_t unResponseBufferSize);
 
-	/*Ces fonctions gèrent la position (rotation quaternionique et position par vecteur) de l'objet,
-	* qrotation est responsable de l'angle et Vecposition[3] de la position.
-	*/
+	///Ces fonctions gèrent la position (rotation quaternionique et position par vecteur) de l'objet,qrotation est responsable de l'angle et Vecposition[3] de la position.
+		/// <summary>
+		/// récupère les touches de clavier, utilisé par la versions stub de GetPose uniquement
+		/// </summary>
+		virtual void GetKeypresses();
+		/// <summary>
+		/// appelle GetKeypresses() et récupère les informations d'angle d'euler et de position puis en fait une pose.
+		/// </summary>
+		/// <returns> Une pose Steamvr DriverPose_t</returns>
+		virtual DriverPose_t GetPoseStubmode();	//stubbed
+		/// <summary>
+		/// recupère les informations locales de quaternion et de position (obtenus via le tunnel nommé normalement) et en fait une pose
+		/// </summary>
+		/// <returns> Une pose Steamvr DriverPose_t</returns>
+		virtual DriverPose_t GetPoseProvided();	// "vraie" version
+		
+		/// <summary>
+		/// version forcée par SteamVR et pas utilisée (décrite dans interfaceRequires.cpp), ne devrait pas être utilisée
+		/// </summary>
+		/// <returns> Une pose Steamvr DriverPose_t</returns>
+		[[maybe_unused]] virtual DriverPose_t GetPose();
 	
-	/// <summary>
-	/// récupère les touches de clavier, utilisé par la versions stub de GetPose uniquement
-	/// </summary>
-	virtual void GetKeypresses();
-	virtual DriverPose_t GetPoseStubmode();	//stubbed
-	virtual DriverPose_t GetPoseProvided();	// "vraie" version
-
-	[[maybe_unused]] virtual DriverPose_t GetPose();	//version forcée par SteamVR et pas utilisée (décrite dans interfaceRequires.cpp)
-	/**
-	*Cette fonction gère la mise à jour des valeurs d'entrée à chaque frame du jeu
-	*/
-	virtual void RunFrameStub();
-	virtual void RunFrameRaw(string raw);
-	virtual std::string GetSerialNumber();
-	/**
-	* Cette fonction gère la mise à jour de valeurs par celles reçues du tunnel nommé
-	*/
-	virtual vector<string> UpdateInternalValuesFromPipedData(string pipeData);
+	///Les fonctions ici gèrent la mise à jour des données à chaque frame-----------------------------------------------------------------
+		/// <summary>
+		/// Cette fonction gère la mise à jour des valeurs d'entrée à chaque frame du jeu (version stub)
+		/// </summary>
+		virtual void RunFrameStub();
+		/// <summary>
+		/// Cette fonction gère la mise à jour des valeurs d'entrée à chaque frame du jeu
+		/// </summary>
+		/// <param name="raw">Un string contenant toutes les valeurs relatives à cet appareil (trame de driver standard)</param>
+		virtual void RunFrameRaw(string raw);
+	///fonctions requises par l'interface de SteamVR (peut être non-utilisé)---------------------------------------------------------------
+		/// <summary>
+		/// Requis par SteamVR, renvoie l'identifiant unique de l'appareil (décrit dans InterfaceRequires.h)
+		/// </summary>
+		/// <returns>L'identifiant de l'appareil</returns>
+		virtual std::string GetSerialNumber();
+	///fonction de mise à jour des valeurs internes
+		/// <summary>
+		/// Cette fonction gère la mise à jour de valeurs par celles reçues du tunnel nommé (appellée par RunFrameRaw())
+		/// </summary>
+		/// <param name="pipeData">la trame contenant toutes les valeurs relatives à cet appareil</param>
+		/// <returns>les valeurs de chaque composant, avec les valeurs relatives à l'appareil (latence, orientation) retirées car déjà appliquées</returns>
+		virtual vector<string> UpdateInternalValuesFromPipedData(string pipeData);
 };
--- source/controller_sim/controller_sim/utilities.h
+++ source/controller_sim/controller_sim/utilities.h
@@ -1,10 +1,28 @@
+/**
+* @author Yorick Geoffre
+* @brief describes various utilities, specifically string-related ones
+* @version 0.1
+* @date 16/12/2021
+*/
+
 #pragma once
 #include "includes.hpp"
 
 //-----------------------------------------Utilitaires---------------------------------------------
 
 namespace utilities {
+	/// <summary>
+	/// permet de séparer un string selon un délilimteur précisé
+	/// </summary>
+	/// <param name="input"> le string à séparer</param>
+	/// <param name="splitter"> le caractère de séparation</param>
+	/// <returns>un vecteur contenant les différentes parties du string d'origine</returns>
 	vector<string> split(string input, char splitter);
+	/// <summary>
+	/// Va prendre le premier string d'un vecteur de string, le retirer de ce vecteur, et le retourner
+	/// </summary>
+	/// <param name="source">le vecteur de strings à traiter</param>
+	/// <returns>le premier string du vecteur</returns>
 	string getFirstAndRemoveFromVector(vector<string> source);
 	HmdQuaternion_t* stringToQuaternion(string input, char splitter);
 	int stringToInt(string input);
