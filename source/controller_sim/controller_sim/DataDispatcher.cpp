2db5ed6939bae7901ffe58839b8f0edba88850f9
Onyx ab3q <Onyxab3q@outlook.com>
2022-01-16 11:12:41 +0100

    added pipe code to driver

--- source/controller_sim/controller_sim/DataDispatcher.cpp
+++ source/controller_sim/controller_sim/DataDispatcher.cpp
@@ -3,73 +3,22 @@
 namespace dispatchers{
 
 	DataDispatcher::DataDispatcher() {
-
+		this->localServer = new PipeServer(this->pipeName);
 	}
 
 	DataDispatcher::~DataDispatcher() {
-		if (this->isPipeConnnected() && this->hPipe != nullptr) {
-			DisconnectNamedPipe(this->hPipe);
-		}
-	}
-
-	bool DataDispatcher::isPipeConnnected() {
-		return false;
-	}
-
-	string DataDispatcher::readPipe() {
-		
-		string data = "";
-		bool success = ReadFile(this->hPipe, &data, 1024, &(this->dwRead), NULL);
-		if (!success) {
-			DriverLog("Error, couldn't read pipe, named pipe may be broken");
-		}
-		return data;
-	}
-
-	bool DataDispatcher::connectPipe() {
-		/*
-		wchar_t* wname = (wchar_t*)malloc(sizeof(wchar_t)* this->pipeName.length());
-		if (wname == NULL) {
-			cerr << "malloc error in dataDispatcher";
-			throw new exception("malloc error in dataDispatcher");
-			exit(1);
-		}
-		size_t* pReturnValue = 0;
-		wchar_t* wcstr = new wchar_t();
-		size_t sizeInWords = 0;
-		const char* mbstr = "tests multiples";
-		size_t count = 10;
-		errno_t errorCode = 0;
-		errorCode =	mbstowcs_s(pReturnValue, wcstr, sizeInWords, mbstr, count);
-		*/
-		LPCSTR ptr = this->pipeName.c_str();
-		this->hPipe = CreateFileA(ptr, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
-		return ConnectNamedPipe(this->hPipe, NULL);
-	}
-
-	bool DataDispatcher::disconnectPipe() {
-		return DisconnectNamedPipe(this->hPipe);
+		delete this->localServer;
 	}
 
 	bool DataDispatcher::feedPipeDataToDrivers(vector<DoMoDriver*> drivers) {
+		this->localServer->WriteToPipe("REQUEST", this->targetName);
+		string anwser = this->localServer->ReadPipe();
+
 		for (DoMoDriver* driver : drivers) {
 			driver->RunFrameStub();
 		}
+
 		return false;
-	}
-
-	vector<string>* DataDispatcher::getPipeData(string raw) {
-		vector<string>* toReturn = new vector<string>();
-		if (!isPipeConnnected()) {
-			throw exception("broken pipe");
-			exit(0);
-		}
-
-		string rawData = readPipe();
-
-		*toReturn = utilities::split(rawData, '|');
-
-		return toReturn;
 	}
 
 }
--- source/controller_sim/controller_sim/DataDispatcher.h
+++ source/controller_sim/controller_sim/DataDispatcher.h
@@ -5,6 +5,7 @@
 
 
 #include "controller_sim.h"
+#include "IPCIPE/IPCServer.h"
 
 using namespace std;
 using namespace vr;
@@ -14,20 +15,13 @@ namespace dispatchers {
 	{
 	private:
 		vector<string> splitData;
-		vector<string>* getPipeData(string RawPipeData);
-		string pipeName = "\\\\.\\domocap\\DriverPipe";
-
-		HANDLE hPipe = nullptr;
-		DWORD dwRead = 0;
+		string pipeName = "\\\\.\\pipe\\DriverPipe";
+		string targetName = "\\\\.\\pipe\\MoulinettePipe";
+		PipeServer* localServer;
 
 	public:
 		DataDispatcher();
 		~DataDispatcher();
-
-		virtual bool isPipeConnnected();
-		virtual bool connectPipe();
-		virtual bool disconnectPipe();
-		virtual string readPipe();
 
 		virtual bool feedPipeDataToDrivers(vector<DoMoDriver*> drivers);
 	};
