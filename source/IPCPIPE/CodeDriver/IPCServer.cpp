433cc63b08fe39cc47e96b608fecd0cf4b76084e
Keturss <mattoluge@gmail.com>
2022-01-27 19:32:22 +0100

    Correctif message erreur

--- source/IPCPIPE/CodeDriver/IPCServer.cpp
+++ source/IPCPIPE/CodeDriver/IPCServer.cpp
@@ -42,7 +42,7 @@ std::string PipeServer::ReadPipe() {
 	if (this->connected) {
 		cout << "was connected, disconnecting...\n";
 		bool  success = DisconnectNamedPipe(this->hPipe);
-		cout << success ? "Successfully disconnected" : "Error disconnecting" + GetLastErrorAsString();
+		cout << (success ? "Successfully disconnected\n" : "Error disconnecting" + GetLastErrorAsString() +"\n");
 	}
 	else {
 		cout << "was not connected, not disconnecting...\n";
@@ -72,7 +72,7 @@ bool PipeServer::WriteToPipe(std::string message, string targetPipe = "") {
 	}
 	this->externPipe = CreateFileA(lpcStr, GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
 	this->isExternConnected = (this->externPipe != INVALID_HANDLE_VALUE);
-	cout << (this->isExternConnected) ? "Connected to distant pipe\n" : "not connected to distant pipe!\n";
+	cout << (this->isExternConnected ? "Connected to distant pipe\n" : "not connected to distant pipe!\n");
 
 	if (this->isExternConnected)
 	{
