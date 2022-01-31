59f09e1947684022b5768c869a7e394ee1fe0340
Kanken6174 <yorick.geoffre@orange.fr>
2022-01-31 10:43:57 +0100

    added SerialPort sources

---
+++ source/Moulinette_Δ/SerialPort/serialPort.h
@@ -0,0 +1,27 @@
+﻿namespace serialport
+{
+class SerialPort
+{
+public:
+	SerialPort();
+	~SerialPort();
+
+
+	bool open(const char* portname, int baudrate, char databit);
+
+
+	void close();
+
+
+	int send(const void* buf, int len);
+
+
+	int receive(void* buf, int maxlen);
+
+	SerialPort connect(char* p, int b);
+
+private:
+	int pHandle[16];
+};
+
+}
