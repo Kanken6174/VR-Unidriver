187c72bf5daf2cb0685feb02cad24d66e5f358f6
jogadet <Jordan.GADET@etu.uca>
2022-02-06 18:16:41 +0100

    Fonction pas en etat de marche + légère refonte graphique et ergonomique

---
+++ source/CCT/CCT_DoMoCap/CCT_DoMoCap/QuaternionComponentUC.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CCT_DoMoCap
+{
+    /// <summary>
+    /// Logique d'interaction pour QuaternionComponentUC.xaml
+    /// </summary>
+    public partial class QuaternionComponentUC : UserControl
+    {
+        public QuaternionComponentUC()
+        {
+            InitializeComponent();
+            ListeBox_AddItem();
+        }
+
+        private void ListeBox_AddItem()
+        {
+            LBGen.Items.Add("Test d'ajout à la listbox");
+            LBGen.Items.Add("Et celui ci ?");
+            LBGen.Items.Add("Un dernier pour le détour");
+            LBGen.Items.Add("Remplissage");
+            LBGen.Items.Add("Remplissage2");
+            LBGen.Items.Add("Remplissage3");
+            LBGen.Items.Add("Remplissage4");
+
+        }
+
+        private void ChangeLetter(KeyEventArgs e)
+        {
+            if (e.Key >= Key.A || e.Key <= Key.Z)
+            {
+                LetterBox.Content = e.Key.ToString();
+            }
+        }
+
+
+
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+
+            if (LetterBox.IsPressed == true && LetterBox.IsFocused == true)
+            {
+                if (Keyboard.IsKeyDown(e.Key))
+                {
+                    ChangeLetter(e);
+                }
+
+            }
+
+        }
+
+    }
+}
