a275157ad39228f58a6e326ed34ea92b7dce57f5
jogadet <Jordan.GADET@etu.uca>
2022-02-07 12:05:39 +0100

    changement branche Bugged -> Main

---
+++ source/CCT/CCT_DoMoCap/CCT_DoMoCap/GenericComponentUC.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CCT_DoMoCap
+{
+    /// <summary>
+    /// Logique d'interaction pour GenericComponentUC.xaml
+    /// </summary>
+    public partial class GenericComponentUC : UserControl
+    {
+        public GenericComponentUC()
+        {
+            InitializeComponent();
+            ListeBox_AddItem();
+
+        }
+
+
+        private void ListeBox_AddItem()
+        {
+            LBGen.Items.Add("Boolean");
+            LBGen.Items.Add("Relatif");
+            LBGen.Items.Add("Absolu");
+
+
+        }
+
+
+        private void ChangeLetter( KeyEventArgs e)
+        {
+           if (e.Key >= Key.A || e.Key <= Key.Z)
+            {
+                LetterBox.Content = e.Key.ToString();
+            }
+        }
+
+
+
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+
+            if (LetterBox.IsChecked == true && LetterBox.IsFocused == true)
+            {
+
+               ChangeLetter(e);
+               
+
+            }
+
+        }
+    }
+}
