9d5045513036d726dc5071e99230fc78ddd277cc
Elliott Le Guéhennec <elliott.LE_GUEHENNEC@etu.uca.fr>
2022-02-07 08:47:43 +0100

    corrected Madgwick library

---
+++ source/Madgwick Rotations/Madgwick Rotations/Quaternion.h
@@ -0,0 +1,98 @@
+/**
+ * @file Quaternion.h
+ * @author Elliott Le Guéhennec(elliott.le_guehennec@etu.uca.fr)
+ * @brief Definition of struct Quaternion, which represents a manipulatable Quaternion
+ * @version 0.1
+ * @date 2021-12-13
+ */
+#pragma once
+
+#include "madgwickRotations.h"
+
+/**
+ * @brief Quaternion used to represent a rotation.
+ * A quaternion is a hyper complex number, containing 1 real dimension and 3 imaginary ones (i, j, k)
+ * 
+ */
+struct madgwickRotations::Quaternion {
+public: //constructors
+	/**
+	 * @brief Construct a new Quaternion object
+	 * 
+	 * @param x Value for x
+	 * @param y Value for y
+	 * @param z Value for z
+	 * @param w Value for w
+	 */
+	Quaternion(double x, double y, double z, double w) : x(x), y(y), z(z), w(w) {}
+
+	/**
+	 * @brief Construct a null Quaternion object
+	 * 
+	 */
+	Quaternion() : x(0), y(0), z(0), w(0) {}
+
+public: //methods defined in Quaternion.cpp
+	/**
+	 * @brief Returns self's conjugate. 
+	 * If a quaternion can be expressed as RE + IM, its conjugate is RE - IM
+	 * 
+	 * @return Quaternion conjugate
+	 */
+	Quaternion conjuguate();
+
+	/**
+	 * @brief Returns self's magnitude.
+	 * The magnitude of a quaternion defined as Q = a + bi + cj + dk is a² + b² + c² + d²
+	 * 
+	 * @return double magnitude
+	 */
+	double magnitude();
+	Quaternion reciprocal();
+
+	/**
+	 * @brief Multiply self by a real number
+	 * 
+	 * @param value factor for the multiplication
+	 * @return Quaternion result of the multiplication
+	 */
+	Quaternion multiplyByReal(double value);
+
+	/**
+	 * @brief returns - Quaternion
+	 * 
+	 * @return Quaternion negative
+	 */
+	Quaternion negative();
+
+private: //coordinates of a quaternion
+	/**
+	 * @brief the real part
+	 * 
+	 */
+	double x;
+
+	/**
+	 * @brief imaginary value: i
+	 * 
+	 */
+	double y;
+
+	/**
+	 * @brief imaginary value: j
+	 * 
+	 */
+	double z;
+
+	/**
+	 * @brief imaginary value: k
+	 * 
+	 */
+	double w; 
+
+public: //getters for the coordinates
+	double getX() { return x; }
+	double getY() { return y; }
+	double getZ() { return z; }
+	double getW() { return w; }
+};
---
+++ source/Madgwick Rotations/Madgwick Rotations/Vector.h
@@ -0,0 +1,64 @@
+/**
+ * @file Vector.h
+ * @author elliott.le_guehennec@etu.uca.fr
+ * @brief Defines struct Vector, which is used as input.
+ * @version 0.1
+ * @date 2021-12-13
+ */
+#pragma once
+
+#include "madgwickRotations.h"
+
+/**
+ * @brief A three-dimensional vector representing the forces measured by an instrument (e.g. gyroscope).
+ * 
+ */
+struct madgwickRotations::Vector {
+public: //constructors
+	/**
+	 * @brief Construct a new Vector object
+	 * 
+	 * @param x Value for x
+	 * @param y Value for y
+	 * @param z Value for z
+	 */
+	Vector(double x, double y, double z) : x(x), y(y), z(z) {}
+
+	/**
+	 * @brief Construct a null Vector object
+	 * 
+	 */
+	Vector() : x(0), y(0), z(0) {}
+	
+public: //methods defined in Vector.cpp
+	/**
+	 * @brief Used for quaternion multiplication. A quaternion and a vector cannot just be multiplied, therefore we convert our vector, setting its W value to 0, so that it is compatible for multiplication.
+	 * 
+	 * @return Quaternion version of self
+	 */
+	Quaternion toQuaternion(); //converts vector to a quaternion
+
+private: //coordinates of a vector
+	/**
+	 * @brief Value on axis x
+	 * 
+	 */
+	double x;
+	
+	/**
+	 * @brief Value on axis y
+	 * 
+	 */
+	double y;
+
+	/**
+	 * @brief Value on axis z
+	 * 
+	 */
+	double z;
+
+public: //getters fpr the coordinates
+	double getX() { return x; }
+	double getY() { return y; }
+	double getZ() { return z; }
+};
